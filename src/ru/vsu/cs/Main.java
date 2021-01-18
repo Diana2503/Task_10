653e7f6e57658df3d096ae02eab6cd2a4319b1ef
Diana2503 <71270469+Diana2503@users.noreply.github.com>
2021-01-18 13:10:47 +0300

    Update Main.java

--- src/ru/vsu/cs/Main.java
+++ src/ru/vsu/cs/Main.java
@@ -7,7 +7,7 @@ import static java.lang.Integer.parseInt;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        List<Tablet> list = FileReader.outputTabletsList(args[0]);
+        List<Tablet> list = FileReader.readFile(args[0]);
         try {
             Tablet result = Sort.findTheMostSuitableTablet(list, parseInt(args[2]));
             FileWriter.writeFile(result, args[1]);
